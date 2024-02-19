be6d3a30afb228ae086f88fb3e7cf8853528017b
ShenaliJayasinghe <shenali.20221088@iit.ac.lk>
2024-02-19 23:07:37 +0530

    Implemented chatbot UI partially and there are errors to be corrected.

---
+++ lib/chatbot_ui/core/app_export.dart
@@ -0,0 +1,8 @@
+export 'package:shenali_s_application1/core/utils/image_constant.dart';
+export 'package:shenali_s_application1/core/utils/size_utils.dart';
+export 'package:shenali_s_application1/routes/app_routes.dart';
+export 'package:shenali_s_application1/theme/app_decoration.dart';
+export 'package:shenali_s_application1/theme/custom_text_style.dart';
+export 'package:shenali_s_application1/theme/theme_helper.dart';
+export 'package:shenali_s_application1/widgets/custom_image_view.dart';
+export 'package:shenali_s_application1/core/utils/date_time_utils.dart';
---
+++ lib/chatbot_ui/core/utils/image_constant.dart
@@ -0,0 +1,21 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // chatbot images
+  static String imgArrowDown = '$imagePath/img_arrow_down.svg';
+
+  static String imgA59568969729 = '$imagePath/img_a5956896_9729.png';
+
+  static String imgSettings = '$imagePath/img_settings.svg';
+
+  static String imgGrid = '$imagePath/img_grid.svg';
+
+  static String imgGallerySvgrepoCom = '$imagePath/img_gallery_svgrepo_com.svg';
+
+  static String imgQuestionMarkC = '$imagePath/img_question_mark_c.svg';
+
+  static String imgSendsvgrepocom = '$imagePath/img_sendsvgrepocom.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
