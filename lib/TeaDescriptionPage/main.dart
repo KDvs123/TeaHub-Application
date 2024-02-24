0a3f5a2481c6366162bd247eae8ebab4960de74d
ShenaliJayasinghe <shenali.20221088@iit.ac.lk>
2024-02-24 12:31:24 +0530

    Handled some errors in tea description page.

--- lib/TeaDescriptionPage/main.dart
+++ lib/TeaDescriptionPage/main.dart
@@ -1,4 +1,4 @@
-import 'package:api/teaDetails.dart';
+import 'package:chatbotui/TeaDescriptionPage/teaDetails.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
