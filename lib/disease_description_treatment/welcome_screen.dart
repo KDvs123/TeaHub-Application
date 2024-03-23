b20c0e8a78088687db705632a98950415c954696
sutzz <yenulibimanya@gmail.com>
2024-03-23 21:00:44 +0530

    implemented the welcome screen of the disease detection and treatment plan pages with a slider from scratch.

--- lib/disease_description_treatment/welcome_screen.dart
+++ lib/disease_description_treatment/welcome_screen.dart
@@ -1,66 +1,109 @@
-import 'package:TeaHub/disease_description_treatment/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:TeaHub/disease_description_treatment/home_screen.dart';
 import 'package:TeaHub/disease_description_treatment/colors.dart';
 
-class WelcomeScreen extends StatelessWidget {
-  final String disease;
-  const WelcomeScreen({Key? key, required this.disease}) : super(key: key);
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  double _dragExtent = 0.0; 
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: colors.whiteClr,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            disease,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1,
-              wordSpacing: 1,
+    return Scaffold(
+      backgroundColor: colors.whiteClr,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min, 
+          children: [
+            Text(
+              "The disease is",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+              ),
             ),
-          ),
-          Image.asset(
-            "assets/images/disease_description_and_treatment_imgs/welcome.jpg",
-            fit: BoxFit.cover,
-            scale: 1.2,
-          ),
-          SizedBox(height: 50),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeScreen(),
-                  ));
-            },
-            child: Container(
-              padding: EdgeInsets.all(14),
+            Text(
+              "Brown Blight Disease",
+              style: TextStyle(
+                fontSize: 34, // Increased font size
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+                color: Color(0xFF4ECB81),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
               decoration: BoxDecoration(
-                color: colors.grnClr,
+                border: Border.all(color: Color(0xFF4ECB81), width: 4), 
                 shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 6,
-                    spreadRadius: 4,
-                  ),
-                ],
               ),
-              child: Text(
-                "GO",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500),
+              child: ClipOval(
+                child: Image.asset(
+                  "assets/images/disease_description_and_treatment_imgs/welcome.jpg",
+                  fit: BoxFit.cover,
+                  width: 300, 
+                  height: 300, 
+                ),
               ),
             ),
-          )
-        ],
+            SizedBox(height: 50), 
+            GestureDetector(
+              onHorizontalDragUpdate: (details) {
+                setState(() {
+                  _dragExtent += details.primaryDelta ?? 0;
+                  _dragExtent = _dragExtent.clamp(0.0, MediaQuery.of(context).size.width - 240.0);
+                });
+              },
+              onHorizontalDragEnd: (details) {
+                if (_dragExtent >= MediaQuery.of(context).size.width - 240.0) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                }
+                setState(() {
+                  _dragExtent = 0.0;
+                });
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Color(0xFF4ECB81).withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        "Slide to Continue",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                    Positioned(
+                      left: _dragExtent,
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, offset: Offset(1, 1), blurRadius: 2),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
